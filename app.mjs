80323142017121f441465540d0b48a7189a3613c
clairekim6872 <clairekim6872@gmail.com>
2023-11-12 19:51:50 -0500

    Updating files

--- app.mjs
+++ app.mjs
@@ -1,33 +1,71 @@
 import './config.mjs';
 import express from 'express'
+import SpotifyWebApi from 'spotify-web-api-node';
 import path from 'path'
 import './db.mjs';
 import mongoose from 'mongoose';
 import { fileURLToPath } from 'url';
 
+// setting up express
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-// set up express static
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }));
 
 // configure templating to hbs
 app.set('view engine', 'hbs');
 
-// body parser (req.body)
-app.use(express.urlencoded({ extended: false }));
-
 // database collections
 const User = mongoose.model('User');
 const Playlist = mongoose.model('Playlist');
+const Artist = mongoose.model('Artist');
+
+// setting up spotify api
+// https://www.npmjs.com/package/spotify-web-api-node#fetch-music-metadata
+const spotifyApi = new SpotifyWebApi({
+    clientId: 'e5445e19f4ab47c49c125f3e81bd2fb3',
+    clientSecret: '643dc358b4a64d66a8c6a98380ca07be'
+});
+
+// retrieve spotify access token
+spotifyApi.clientCredentialsGrant().then(
+    function (data) {
+        // console.log('The access token expires in ' + data.body['expires_in']);
+        // console.log('The access token is ' + data.body['access_token']);
+        const token = data.body['access_token'];
+        spotifyApi.setAccessToken(token);
+    },
+    function (err) {
+        console.log('Something went wrong when retrieving an access token', err);
+    }
+);
 
 app.get('/', async (req, res) => {
+    await Artist.deleteMany({});
+
     const key = {};
+    const titles = [];
+    const noDupes = [];
 
     Playlist.find(key)
         .then(playlists => {
-            res.render('home', { playlists });
+            playlists.map(item => {
+                if (noDupes.includes(item.playlistName) === false) {
+                    noDupes.push(item.playlistName)
+                    const plName = item.playlistName;
+                    const obj = { 'playlistName': plName, 'info': [item.artists] };
+                    titles.push(obj)
+                }
+                else {
+                    const target = titles.filter(x => x.playlistName === item.playlistName);
+                    const obj = item.artists;
+                    target[0].info.push(obj)
+                }
+            });
+
+            // titles.map(x => console.log(x))
+            res.render('home', { titles });
         })
         .catch(() => res.status(500).send("Server Error"));
 });
@@ -40,10 +78,6 @@ app.post('/register', (req, res) => {
     // handle form data for registering and logging in
 });
 
-app.get('/spotify-profile-demo/index.html', (req, res) => {
-    
-}); 
-
 app.get('/create', (req, res) => {
     // created playlists  here
     res.render('create', {});
@@ -51,27 +85,72 @@ app.get('/create', (req, res) => {
 
 app.post('/create', async (req, res) => {
     // handle form data for creating playlists
-    const title = req.body.title;
     const artist = req.body.artist;
-    const songs = [{ title, artist }];
+    let link;
 
-    const p = new Playlist({
-        playlistName: req.body.playlist,
-        songs: songs
-    });
+    // search spotify by artist name and get artist link
+    spotifyApi
+        .searchArtists(artist)
+        .then(data => {
+            link = data.body.artists.items[0]['external_urls'].spotify;
 
-    // saving review to database
-    p.save()
-        .then(() => res.redirect('/'))
+            const p = new Playlist({
+                playlistName: req.body.playlist,
+                artists: { artist: req.body.artist, link: link }
+            });
+
+            // saving playlist to database
+            p.save()
+                .then(() => {
+                    res.redirect('/');
+                })
+                .catch(() => res.status(500).send("Server Error"));
+        })
+        .catch(err => {
+            console.log("The error while searching artists occurred: ", err);
+        });
+});
+
+app.get('/filter', async (req, res) => {
+    // filter albums  here
+    const key = {};
+
+    Artist.find(key)
+        .then(found => {
+            res.render('filter', { found });
+        })
         .catch(() => res.status(500).send("Server Error"));
 });
 
-app.get('/edit', (req, res) => {
-    // edit playlists  here
-});
+app.post('/filter', async (req, res) => {
+    await Artist.deleteMany({});
 
-app.post('/edit', (req, res) => {
-    // handle form data for editing playlists
+    spotifyApi
+        .searchArtists(req.body.artist)
+        .then(data => {
+            const albumList = [];
+            const link = data.body.artists.items[0]['external_urls'].spotify;
+            const artistID = link.split('/')[4];
+
+            spotifyApi.getArtistAlbums(artistID).then(data => {
+                data.body.items.map(x => {
+                    albumList.push(x.name)
+                });
+
+                let query = { artistName: req.body.artist };
+                let update = {
+                    $setOnInsert: {
+                        artistName: req.body.artist, albums: albumList
+                    }
+                };
+                let options = { upsert: true };
+
+                Artist.findOneAndUpdate(query, update, options)
+                    .catch(error => console.error(error));
+
+                res.redirect('/filter');
+            });
+        });
 });
 
 app.listen(process.env.PORT || 3000);
--- db.mjs
+++ db.mjs
@@ -6,10 +6,7 @@ mongoose.connect(process.env.DSN);
 const Playlist = new mongoose.Schema({
     username: String,
     playlistName: String,
-    songs: [{
-        title: String,
-        artist: String,
-    }]
+    artists: { artist: String, link: String },
 });
 
 const User = new mongoose.Schema({
@@ -18,6 +15,12 @@ const User = new mongoose.Schema({
     playlist: [Playlist]
 });
 
+const Artist = new mongoose.Schema({
+    artistName: String,
+    albums: [String]
+});
+
 // "register" it so that mongoose knows about it
 mongoose.model('User', User);
+mongoose.model('Artist', Artist);
 mongoose.model('Playlist', Playlist);
